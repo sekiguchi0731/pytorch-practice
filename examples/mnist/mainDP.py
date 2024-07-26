f9b22fa708db08507cafc4f16c76231d8231bfe7
sekiguchi0731 <seki.guchi.3725@gmail.com>
2024-07-26 19:24:22 +0900

    CDP-SGDの実装

--- examples/mnist/mainDP.py
+++ examples/mnist/mainDP.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 from typing import Any
 
 import matplotlib.pyplot as plt
@@ -12,11 +13,13 @@ from torchvision import datasets, transforms
 
 
 class PrivacyParams:
-    def __init__(self, eps: float = 0.5, sensitivity: float = 1.0, mean: float = 0) -> None:
+    def __init__(
+        self, eps: float = 0.5, delta: float = 0.0001, mean: float = 0
+    ) -> None:
         self.eps: float = eps
-        self.sensitivity: float = sensitivity
+        self.delta: float = delta
         self.mean: float = mean
-        self.var: float = self.sensitivity / self.eps
+        self.var: float = math.sqrt(2 * math.log(1.25 / self.delta)) / self.eps
 
 
 class Net(nn.Module):
@@ -43,99 +46,76 @@ class Net(nn.Module):
 
 
 # 勾配のクリッピング
-## modelとclipping_normを受け取り、model内の勾配を直接変更
-def clip_gradients(model, clipping_norm=1.0) -> None:
-    # 全てのパラメータについて勾配をクリッピング
-    for p in model.parameters():
-        if p.grad is not None:
-            # print(p.grad[0])
-            param_norm: torch.Tensor = p.grad.data.norm(2)
-            clip_coef: torch.Tensor = clipping_norm / (param_norm + 1e-6)
-            if clip_coef < 1:
-                p.grad.data.mul_(clip_coef)
+def clip_gradients(grads, clipping_norm=1.0) -> list[torch.Tensor]:
+    clipped_grads: list[torch.Tensor] = []
+    for grad in grads:
+        param_norm: torch.Tensor = grad.norm(2)
+        clip_coef: torch.Tensor = clipping_norm / (param_norm + 1e-6)
+        if clip_coef < 1:
+            grad: torch.Tensor = grad.clone() * clip_coef.to(grad.device)
+        clipped_grads.append(grad)
+    return clipped_grads
 
 
-# 勾配の平均化
-def average_gradients(grads_per_data: list[list[torch.Tensor]]) -> list[torch.Tensor]:
-    num_data_points: int = len(grads_per_data)
-    num_params: int = len(grads_per_data[0])
-    avg_grads: list[torch.Tensor] = []
-
-    for i in range(num_params):
-        for j in range(num_data_points):
-            stacked_grads: torch.Tensor = torch.stack([grads_per_data[j][i]])
-        avg_grad: torch.Tensor = torch.mean(stacked_grads, dim=0)
-        avg_grads.append(avg_grad)
-
-    return avg_grads
+# ラプラスノイズの追加
+def add_laplace_noise(model, grads) -> list[torch.Tensor]:
+    privacy_params: PrivacyParams = model.privacy_params
+    noised_grads: list[torch.Tensor] = []
+    for grad in grads:
+        noise: torch.Tensor = torch.distributions.Laplace(
+            privacy_params.mean, privacy_params.var
+        ).sample(grad.shape)
+        noised_grads.append(grad + (noise / math.sqrt(len(grads))).to(grad.device))
+    return noised_grads
 
 
 def train(args, model, device, train_loader, optimizer, epoch, is_Scaling) -> None:
     model.train()
-    for batch_idx, (dataset, targets) in enumerate(train_loader):
-        dataset, targets = dataset.to(device), targets.to(device)
-        output: torch.Tensor = model(dataset)
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+
+        # バッチ全体の出力を取得
+        output: torch.Tensor = model(data)
+        loss: torch.Tensor = F.nll_loss(output, target, reduction="none")
+
+        # 勾配のリストを初期化
         grads_per_data: list[list[torch.Tensor]] = []
-        for idx, data in enumerate(dataset):
-            optimizer.zero_grad()
-            loss: torch.Tensor = F.nll_loss(output[idx], targets[idx])
-            # 計算グラフを保持：dataが複数ある場合に、複数回backwardを計算できないから。
-            loss.backward(retain_graph=True)
-            # print(loss)
-            clip_gradients(model)
+
+        # 各データポイントごとに勾配を計算
+        for i in range(len(data)):
+            model.zero_grad()
+            loss[i].backward(retain_graph=True)
             grads: list[torch.Tensor] = [
                 p.grad.clone() for p in model.parameters() if p.grad is not None
             ]
             grads_per_data.append(grads)
 
-        # print(len(grads_per_data[0][0]))
-        # 勾配の平均化
-        # 勾配の平均化
-        avg_grads: list[torch.Tensor] = average_gradients(grads_per_data)
-        # print(len(avg_grads))
+        # 各データポイントの勾配をクリッピング
+        # 64x8x([32,1,3,3],[32],...)
+        clipped_grads_per_data: list[list[torch.Tensor]] = [
+            clip_gradients(grads) for grads in grads_per_data
+        ]
 
-        for param, avg_grad in zip(model.parameters(), avg_grads):
-            if param.grad is not None:
-                param.grad = avg_grad
-
-        # optimizer.zero_grad()
-        # output: torch.Tensor = model(data)
-        # losses: torch.Tensor = F.nll_loss(output, target, reduction='none')
-        # print(losses)
-        # print(len(data))
-        # for loss in losses:
-        #     loss.backward()
-        #     # 勾配のクリッピング
-        #     clip_gradients(model)
-        # # 計算後の勾配を保存
-        # grads: list[torch.Tensor] = [
-        #     param.grad.clone()
-        #     for param in model.parameters()
-        #     if param.grad is not None
-        # ]
-
+        # クリッピングされた勾配の平均を計算
+        avg_grads: list[torch.Tensor] = [
+            torch.mean(
+                torch.stack([grads[i] for grads in clipped_grads_per_data]), dim=0
+            )
+            for i in range(len(clipped_grads_per_data[0]))
+        ]
 
         # ノイズの追加
-        # noised_grads: list[torch.Tensor] = []
-        # privacy_params: PrivacyParams = model.privacy_params
+        noised_grads: list[torch.Tensor] = add_laplace_noise(model, avg_grads)
 
-        # for grad in clipped_grads:
-        #     if is_Scaling:
-        #         noise: torch.Tensor = torch.distributions.Laplace(
-        #             privacy_params.mean, privacy_params.var
-        #         ).sample(grad.shape) / math.sqrt(len(grads))
-        #     else:
-        #         noise: torch.Tensor = torch.distributions.Laplace(
-        #             privacy_params.mean, privacy_params.var
-        #         ).sample(grad.shape)
-        #     noised_grads.append(grad + noise)
+        # 平均化された勾配をモデルに適用
+        for param, noised_grad in zip(model.parameters(), noised_grads):
+            if param.grad is not None:
+                param.grad.data.copy_(noised_grad)  # grad を直接コピーする
+            # param.grad.data = noised_grad
 
-        # オプティマイザのパラメータに手動で勾配を設定
-        # for param, grad in zip(model.parameters(), noised_grads):
-        #     param.grad = grad.to(device)
-
-        # パラメータの更新
         optimizer.step()
+
         if batch_idx % args.log_interval == 0:
             print(
                 "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
@@ -143,7 +123,7 @@ def train(args, model, device, train_loader, optimizer, epoch, is_Scaling) -> No
                     batch_idx * len(data),
                     len(train_loader.dataset),
                     100.0 * batch_idx / len(train_loader),
-                    loss.item(),
+                    loss.mean().item(),
                 )
             )
             if args.dry_run:
@@ -152,19 +132,19 @@ def train(args, model, device, train_loader, optimizer, epoch, is_Scaling) -> No
 
 def test(model, device, test_loader) -> int:
     model.eval()
-    test_loss: float = 0
-    correct: int = 0
+    test_loss = 0
+    correct = 0
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
-            output: torch.Tensor = model(data)
+            output = model(data)
             test_loss += F.nll_loss(
                 output, target, reduction="sum"
             ).item()  # sum up batch loss
-            pred: torch.Tensor = output.argmax(
+            pred = output.argmax(
                 dim=1, keepdim=True
             )  # get the index of the max log-probability
-            correct += int(pred.eq(target.view_as(pred)).sum().item())
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
 
@@ -183,7 +163,7 @@ def plot_graph(
     args,
     eps_values: list[float],
     all_correct_lists_without_scaling: list[list[int]],
-    all_correct_lists_with_scaling: list[list[int]],
+    all_correct_lists_with_scaling: list[list[int]] = [],
 ) -> None:
     # グラフの作成
     fig, axes = plt.subplots(5, 2, figsize=(15, 25))
@@ -211,13 +191,13 @@ def plot_graph(
             range(1, args.epochs + 1),
             correct_list,
             marker="o",
-            label="Without Scaling",
+            label="With Scaled DP",
         )
         axes[i].plot(
             range(1, args.epochs + 1),
             noDP_list,
             marker="o",
-            label="With Scaling",
+            label="Without DP",
         )
         # axes[i].plot(
         #     range(1, args.epochs + 1),
@@ -233,9 +213,10 @@ def plot_graph(
         axes[i].legend()
         axes[i].grid()
 
-    plt.suptitle(
-        r"Impact of Laplace Mechanism for DP on Model Accuracy with $\sqrt{m}$ Scaling"
-    )
+    plt.suptitle(r"Impact of Laplace Mechanism for DP on Model Accuracy")
+    # plt.suptitle(
+    #     r"Impact of Laplace Mechanism for DP on Model Accuracy with $\sqrt{m}$ Scaling"
+    # )
 
     plt.tight_layout(
         rect=(0, 0, 1, 0.97)
@@ -318,17 +299,12 @@ def main() -> None:
 
     torch.manual_seed(args.seed)
 
-    if use_cuda:
-        device = torch.device("cuda")
-    elif use_mps:
-        device = torch.device("mps")
-    else:
-        device = torch.device("cpu")
+    device = torch.device("cuda" if use_cuda else "mps" if use_mps else "cpu")
 
     train_kwargs: dict[str, Any] = {"batch_size": args.batch_size}
     test_kwargs: dict[str, Any] = {"batch_size": args.test_batch_size}
     if use_cuda:
-        cuda_kwargs: dict[str, Any] = {"num_workers": 1, "pin_memory": True, "shuffle": True}
+        cuda_kwargs = {"num_workers": 1, "pin_memory": True, "shuffle": True}
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
 
@@ -340,72 +316,26 @@ def main() -> None:
     train_loader = DataLoader(dataset1, **train_kwargs)
     test_loader = DataLoader(dataset2, **test_kwargs)
 
-    eps_values: list[float] = [0.1 * i for i in range(1, 3)]
-    # eps_values: list[float] = [0.1 * i for i in range(1, 11)]
-    all_correct_lists_without_scaling: list[list[int]] = []
-    all_correct_lists_with_scaling: list[list[int]] = []
-
+    eps_values: list[float] = [0.5 * i for i in range(1, 2)]
+    all_collect_lists: list[list[int]] = []
     for eps in eps_values:
-        print(eps)
-        privacy_params = PrivacyParams(eps=eps, sensitivity=1.0, mean=0)
-        model_without_scaling: Net = Net(privacy_params).to(device)
-        model_with_scaling: Net = Net(privacy_params).to(device)
-        optimizer_without_scaling = optim.Adadelta(
-            model_without_scaling.parameters(), lr=args.lr
-        )
-        optimizer_with_scaling = optim.Adadelta(
-            model_with_scaling.parameters(), lr=args.lr
-        )
-
-        scheduler_without_scaling = StepLR(
-            optimizer_without_scaling, step_size=1, gamma=args.gamma
-        )
-        scheduler_with_scaling = StepLR(
-            optimizer_with_scaling, step_size=1, gamma=args.gamma
-        )
-
-        correct_list_without_scaling: list[int] = []
-        correct_list_with_scaling: list[int] = []
+        privacy_params = PrivacyParams(eps=eps, delta=0.0001, mean=0)
+        model: Net = Net(privacy_params).to(device)
+        optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+        scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+        collect_list: list[int] = []
 
         for epoch in range(1, args.epochs + 1):
-            train(
-                args,
-                model_without_scaling,
-                device,
-                train_loader,
-                optimizer_without_scaling,
-                epoch,
-                False,
-            )
-            correct: int = test(model_without_scaling, device, test_loader)
-            correct_list_without_scaling.append(correct)
-            scheduler_without_scaling.step()
-            train(
-                args,
-                model_with_scaling,
-                device,
-                train_loader,
-                optimizer_with_scaling,
-                epoch,
-                True,
-            )
-            correct = test(model_with_scaling, device, test_loader)
-            correct_list_with_scaling.append(correct)
-            scheduler_with_scaling.step()
+            train(args, model, device, train_loader, optimizer, epoch, False)
+            collect_list.append(test(model, device, test_loader))
+            scheduler.step()
 
-        all_correct_lists_without_scaling.append(correct_list_without_scaling)
-        all_correct_lists_with_scaling.append(correct_list_with_scaling)
+        if args.save_model:
+            torch.save(model.state_dict(), "mnist_cnn.pt")
 
-    if args.save_model:
-        torch.save(model_without_scaling.state_dict(), "mnist_cnn1.pt")
-        torch.save(model_with_scaling.state_dict(), "mnist_cnn2.pt")
+        all_collect_lists.append(collect_list)
 
-    plot_graph(
-        args,
-        eps_values,
-        all_correct_lists_without_scaling,
-        all_correct_lists_with_scaling,
-    )
+    plot_graph(args, eps_values, all_collect_lists)
 
 
 if __name__ == "__main__":
